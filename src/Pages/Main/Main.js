7200b660323c27ac8e42add3aa8b4d198d95d88a
susmoysaha6 <susmoysaha6@gmail.com>
2022-12-10 18:18:48 +0600

    banner section added

---
+++ src/Pages/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Banner from '../../components/Banner/Banner';
+import Headers from '../../components/Headers/Headers';
+
+
+const Main = () => {
+    return (
+        <div>
+            <Headers></Headers>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div id='#home' className='my-10'>
+            <div className="shadow-2xl">
+                <div className="flex flex-col md:flex-row justify-evenly items-center">
+                    <div>
+                        <h1 className="text-5xl font-bold">Susmoy Saha</h1>
+                        <p className="py-6">Web Developer || MERN Stack Developer</p>
+                    </div>
+                    <div className=' mt-5'>
+                        <img src="https://i.ibb.co/hLq2NQz/15933874-1363042953769435-915479771-o-removebg-preview.png" alt='' className="rounded-lg " />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
